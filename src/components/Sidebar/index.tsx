6ecabe2dec7bd48dee2f436194e0e7ac1003caa2
Felipe Soares Coelho <felipecoelho.ness@gmail.com>
2023-10-15 16:23:15 -0300

    feat(style): sidebar and config basic

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+
+import { BurgerIcon } from "@/assets/burger";
 import { Container } from "./styles";
+import { MenuIcon } from "@/assets/menu";
+import Image from "next/image";
+import menuImg from "@/assets/menu.svg";
+import { BurgerAndSoda } from "@/assets/burger-and-soda";
+import { PizzaIcon } from "@/assets/pizza";
+import { SodaIcon } from "@/assets/soda";
+import { IceCreamIcon } from "@/assets/ice-cream";
 
 export function Sidebar() {
-  return <Container>Sidebar</Container>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <Container isMenuOpen={menuOpen}>
+      <button type="button" onClick={handleToggleMenu}>
+        <Image src={menuImg} alt="Open and close menu" />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href="#" className="active">
+              <BurgerIcon />
+              <span>Hamburgers</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <PizzaIcon />
+              <span>Pizza</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <SodaIcon />
+              <span>Soda</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <IceCreamIcon />
+              <span>IceCream</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  );
 }
